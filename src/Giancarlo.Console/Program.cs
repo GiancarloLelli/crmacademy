8b406862670c3afe0f7ad153f7de67cfc219ead7
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-03-29 12:52:10 +0100

    pushe giancarlo

--- src/Giancarlo.Console/Program.cs
+++ src/Giancarlo.Console/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xrm.Tooling.Connector;
+using System;
+using System.Configuration;
 
-namespace Giancarlo.Console
+namespace Giancarlo.App
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CrmServiceClient crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMONLINE"].ConnectionString);
+            Console.ReadLine();
         }
     }
 }
