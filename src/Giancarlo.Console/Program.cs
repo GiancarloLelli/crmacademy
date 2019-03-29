920ea9415f9875b4d53f76352eb7b4c0810ba9ed
Giancarlo Lelli <gcarlo.lelli@live.com>
2019-03-29 14:22:45 +0100

    added link

--- src/Giancarlo.Console/Program.cs
+++ src/Giancarlo.Console/Program.cs
@@ -6,10 +6,38 @@ namespace Giancarlo.App
 {
     class Program
     {
+        // https://www.microsoft.com/en-us/download/details.aspx?id=50032
+
         static void Main(string[] args)
         {
             CrmServiceClient crmSvc = new CrmServiceClient(ConfigurationManager.ConnectionStrings["CRMONLINE"].ConnectionString);
+
+            CreateRecords(crmSvc);
+            RetrieveRecords(crmSvc);
+            UpdateRecords(crmSvc);
+            DeleteRecords(crmSvc);
+
             Console.ReadLine();
+        }
+
+        private static void DeleteRecords(CrmServiceClient crmSvc)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void UpdateRecords(CrmServiceClient crmSvc)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void RetrieveRecords(CrmServiceClient crmSvc)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void CreateRecords(CrmServiceClient crmSvc)
+        {
+            throw new NotImplementedException();
         }
     }
 }
