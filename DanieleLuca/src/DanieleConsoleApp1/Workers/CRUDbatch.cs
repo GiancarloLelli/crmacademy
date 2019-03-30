c2b540e9b2e85e916edff566ccaf60c43a684fcb
danieleclassic <danieleclassic@gmail.com>
2019-03-30 19:17:35 +0100

    test2

---
+++ DanieleLuca/src/DanieleConsoleApp1/Workers/CRUDbatch.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Avanade.Xrm.Batches;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanieleConsoleApp1.Workers
+{
+    class CRUDbatch : IFlow
+    {
+        public void Execute(IFlowContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- DanieleLuca/src/DanieleLuca.Luca.Console/Workers/CRUDBatch.cs
+++ DanieleLuca/src/DanieleLuca.Luca.Console/Workers/CRUDBatch.cs
@@ -6,74 +6,74 @@ using System;
 
 namespace DanieleLuca.Luca.Console.Workers
 {
-	class CRUDBatch : IFlow
-	{
-		public void Execute(IFlowContext context)
-		{
-			var crmSvc = context.Service;
-			CreateRecords(crmSvc);
+    class CRUDBatch : IFlow
+    {
+        public void Execute(IFlowContext context)
+        {
+            var crmSvc = context.Service;
+            CreateRecords(crmSvc);
 
-			var lastReference=	RetrieveRecords(crmSvc);
-			UpdateRecords(crmSvc, lastReference);
+            var lastReference = RetrieveRecords(crmSvc);
+            UpdateRecords(crmSvc, lastReference);
 
-			lastReference=RetrieveRecords(crmSvc);
-			DeleteRecords(crmSvc, RetrieveRecords(crmSvc));
-		}
+            lastReference = RetrieveRecords(crmSvc);
+            DeleteRecords(crmSvc, RetrieveRecords(crmSvc));
+        }
 
 
-		private void DeleteRecords(IOrganizationService crmSvc, EntityReference reference)
-		{
-			var newEntity = new Entity(reference.LogicalName);
+        private void DeleteRecords(IOrganizationService crmSvc, EntityReference reference)
+        {
+            var newEntity = new Entity(reference.LogicalName);
 
-			crmSvc.Delete("ava_todo", newEntity.Id);
-		}
+            crmSvc.Delete("ava_todo", newEntity.Id);
+        }
 
-		private void UpdateRecords(IOrganizationService crmSvc, EntityReference reference)
-		{
-			var newEntity = new Entity(reference.LogicalName);
-			newEntity.Id = reference.Id;
-			newEntity["ava_name"] = "Pippo";
-			newEntity["ava_done"] = true;
-			crmSvc.Update(newEntity);
-		}
+        private void UpdateRecords(IOrganizationService crmSvc, EntityReference reference)
+        {
+            var newEntity = new Entity(reference.LogicalName);
+            newEntity.Id = reference.Id;
+            newEntity["ava_name"] = "Pippo";
+            newEntity["ava_done"] = true;
+            crmSvc.Update(newEntity);
+        }
 
-		private EntityReference RetrieveRecords(IOrganizationService crmSvc)
-		{
+        private EntityReference RetrieveRecords(IOrganizationService crmSvc)
+        {
 
-			var query = new QueryExpression("ava_todo");
+            var query = new QueryExpression("ava_todo");
 
-			// Si segnalano le varie opzioni tra cui  le colonne da prendere
-			// Query.ColumnSet = new ColumnSet("ava_name"); colonna singola
-			query.ColumnSet = new ColumnSet(true);
-			query.TopCount = 10;
-			query.AddOrder("createdon", OrderType.Descending);
-			query.Criteria.AddCondition("ava_done", ConditionOperator.Equal, false);
+            // Si segnalano le varie opzioni tra cui  le colonne da prendere
+            // Query.ColumnSet = new ColumnSet("ava_name"); colonna singola
+            query.ColumnSet = new ColumnSet(true);
+            query.TopCount = 10;
+            query.AddOrder("createdon", OrderType.Descending);
+            query.Criteria.AddCondition("ava_done", ConditionOperator.Equal, false);
 
-			var entities = crmSvc.RetrieveMultiple(query);
+            var entities = crmSvc.RetrieveMultiple(query);
 
-			//setta riferimento default
-			EntityReference reference = null;
+            //setta riferimento default
+            EntityReference reference = null;
 
-			//ciclo dei record e popola le reference
-			foreach (var item in entities.Entities)
-			{
-				reference = item.ToEntityReference();
-				var name = item.GetAttributeValue<string>("ava_name");
-				System.Console.WriteLine($"NAme:{ name}");
-			}
+            //ciclo dei record e popola le reference
+            foreach (var item in entities.Entities)
+            {
+                reference = item.ToEntityReference();
+                var name = item.GetAttributeValue<string>("ava_name");
+                System.Console.WriteLine($"NAme:{ name}");
+            }
 
-			return reference;
-		}
+            return reference;
+        }
 
-		private void CreateRecords(IOrganizationService crmSvc)
-		{
-			var entity = new Entity("ava_todo");
-			entity["ava_name"] = "TODO-1";
-			entity["ava_duedate"] = DateTime.Now.AddYears(2);
-			entity["ava_startdate"] = DateTime.Now;
-			entity["ava_done"] = false;
+        private void CreateRecords(IOrganizationService crmSvc)
+        {
+            var entity = new Entity("ava_todo");
+            entity["ava_name"] = "TODO-1";
+            entity["ava_duedate"] = DateTime.Now.AddYears(2);
+            entity["ava_startdate"] = DateTime.Now;
+            entity["ava_done"] = false;
 
-			crmSvc.Create(entity);
-		}
-	}
+            crmSvc.Create(entity);
+        }
+    }
 }
